8769149c9be46a23c0e3719f3136f7afe55baea8
Pranjal Rawat <pranjal.rawat@outlook.com>
2026-03-31 08:10:41 -0400

    feat: add Objectworld and Binaryworld environments for Wulfmeier (2016) replication

--- src/econirl/environments/__init__.py
+++ src/econirl/environments/__init__.py
@@ -1,13 +1,17 @@
 """Economic environments with Gymnasium compatibility."""
 
 from econirl.environments.base import DDCEnvironment
+from econirl.environments.binaryworld import BinaryworldEnvironment
 from econirl.environments.gridworld import GridworldEnvironment
 from econirl.environments.multi_component_bus import MultiComponentBusEnvironment
+from econirl.environments.objectworld import ObjectworldEnvironment
 from econirl.environments.rust_bus import RustBusEnvironment
 
 __all__ = [
+    "BinaryworldEnvironment",
     "DDCEnvironment",
     "GridworldEnvironment",
     "MultiComponentBusEnvironment",
+    "ObjectworldEnvironment",
     "RustBusEnvironment",
 ]
